d80ba867912c86a92e135885b49a57f0bdf9c391
Shayne Thiessen <shayne@shaynethiessen.com>
2020-07-14 15:15:34 -0500

    Create Sample Data.tsx

---
+++ Samples/Sample Data.tsx
@@ -0,0 +1,69 @@
+let PortfolioObject = {
+    Gallery: {
+        Photos: [
+            {
+                id: 1,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/ade.jpg',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 2,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/elliot.jpg',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 3,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/kristy.png',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 4,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/nan.jpg',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 5,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/elliot.jpg',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 6,
+                url: 'https://fluentsite.z22.web.core.windows.net/public/images/avatar/large/ade.jpg',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 7,
+                url: 'https://react.semantic-ui.com/images/wireframe/image.png',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 8,
+                url: 'https://react.semantic-ui.com/images/wireframe/image.png',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+            {
+                id: 9,
+                url: 'https://react.semantic-ui.com/images/wireframe/image.png',
+                date: new Date(),
+                caption: 'My Picture',
+                tags: ["Cat", "City", "Nature"]
+            },
+        ]
+    }
+};
